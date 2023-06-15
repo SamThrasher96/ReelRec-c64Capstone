b947807f958c8d1ac48552c32f76be2ac5e6eab5
Sam Thrasher <samthrasher9@gmail.com>
2023-06-15 10:58:15 -0500

    Added a user css page and import it

--- src/components/User/UserProfile.js
+++ src/components/User/UserProfile.js
@@ -1,3 +1,5 @@
+import "./User.css"
+
 export const UserProfile = () => {
     const localReelRecUser = localStorage.getItem("reelRec_user")
     const reelRecUserObject = JSON.parse(localReelRecUser)
