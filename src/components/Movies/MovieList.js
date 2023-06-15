26fcd7b29c3d50c18de6ff273b35840e0c16b5c7
Sam Thrasher <samthrasher9@gmail.com>
2023-06-15 13:51:02 -0500

    First try at movie list code

--- src/components/Movies/MovieList.js
+++ src/components/Movies/MovieList.js
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 
 export const MovieList =({}) => {
     const [movies, setMovies] = useState([])
+    const navigate = useNavigate()
+
+    useEffect(
+        () => {
+            fetch(``)
+                .then(response => response.json())
+                .then((movieArray) => {
+                    setMovies(movieArray)
+                })
+            console.log("Initial state of tickets", tickets)
+        }
+    )
+
+    return <>
+    <h2>List of movies</h2>
+    <article className="movies">
+        {
+            setMovies.map(
+                (movie) => {
+                    return <section className="movie" key={`movie--${movie.id}`}>
+                        <header>Movie title:{movie.name}</header>
+                        <div>Genre: {movie.genreId}</div>
+                        <div>This movie is rated {movie.mpaRatingId}</div>
+                        <div>Streaming Service: {movie.streamingServiceId}</div>
+                        <div>{movie.description}</div>
+                    </section>
+                }
+            )
+        }
+    </article>
+    </>
 }
