ae3c77952da9b354f70e039499b7830d0d7237a5
Sam Thrasher <samthrasher9@gmail.com>
2023-06-28 22:45:36 -0500

    Added remove buttons

--- src/components/Movies/FavoriteList.js
+++ src/components/Movies/FavoriteList.js
@@ -1,40 +1,62 @@
-import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const UserFavoriteList = () => {
-    const [userFavoriteList, setUserFavoriteList] = useState([])
-    const localReelRecUser = localStorage.getItem("reelRec_user")
-    const ReelRecUserObject = JSON.parse(localReelRecUser)
-    const navigate = useNavigate()
+    const [userFavoriteList, setUserFavoriteList] = useState([]);
+    const localReelRecUser = localStorage.getItem("reelRec_user");
+    const ReelRecUserObject = JSON.parse(localReelRecUser);
+    const navigate = useNavigate();
 
-    useEffect(() => {
-        fetch(`http://localhost:8088/userWatchListAndFavorites?_expand=movie`)
-            .then(response => response.json())
-            .then((favoriteListArray) => {
-                const filterFavoriteList = favoriteListArray.filter(item => item.userId === ReelRecUserObject.id && item.favorite === true )
-                setUserFavoriteList(filterFavoriteList)
-                console.log(filterFavoriteList)
-            })
-    },
+useEffect(() => {
+    fetch(`http://localhost:8088/userWatchListAndFavorites?_expand=movie`)
+        .then((response) => response.json())
+        .then((favoriteListArray) => {
+        const filterFavoriteList = favoriteListArray.filter(
+            (item) =>
+            item.userId === ReelRecUserObject.id && item.favorite === true
+        );
+        setUserFavoriteList(filterFavoriteList);
+        console.log(filterFavoriteList);
+        });
+    }, 
     []
 );
 
-return <>
-<h2>Favorite List</h2>
-<button onClick={() => navigate("/")}>Home</button>
+const removeFromFavorites = (favoriteId) => {
+    fetch(`http://localhost:8088/userWatchListAndFavorites/${favoriteId}`, {
+        method: "PATCH",
+        headers: {
+        "Content-Type": "application/json",
+    },
+        body: JSON.stringify({ favorite: false }),
+    }).then(() => {
+        const updatedFavorites = userFavoriteList.filter(
+        (item) => item.id !== favoriteId
+    );
+        setUserFavoriteList(updatedFavorites);
+        console.log("Movie removed from favorites");
+    });
+};
 
-<article className="movies">
-    {userFavoriteList.map(
-        (movie) => {
-            return <section className="movie" key={`userFavoriteList--${movie.id}`}>
+    return (
+    <>
+        <h2>Favorite List</h2>
+        <button onClick={() => navigate("/")}>Home</button>
+
+        <article className="movies">
+        {userFavoriteList.map((movie) => {
+            return (
+            <section className="movie" key={`userFavoriteList--${movie.id}`}>
                 <img src={movie.movie.image} alt={movie.movie.name} />
-                    <header>{movie.movie.name}</header>
-                    <footer>Description: {movie.movie.description}</footer>
+                <header>{movie.movie.name}</header>
+                <footer>Description: {movie.movie.description}</footer>
+                <button onClick={() => removeFromFavorites(movie.id)}>
+                Remove from favorites
+            </button>
             </section>
-        }
-    )}
-</article>
-
-</>
-}
+        );
+        })}
+    </article>
+    </>
+);
+};
--- src/components/Movies/WatchList.js
+++ src/components/Movies/WatchList.js
@@ -20,6 +20,22 @@ export const UserWatchList = () => {
     []
 );
 
+const removeFromWatchList = (watchListId) => {
+    fetch(`http://localhost:8088/userWatchListAndFavorites/${watchListId}`, {
+        method: "PATCH",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify({watchList: false}),
+    }).then(() => {
+        const updatedWatchList = userWatchList.filter(
+            (item) => item.id !== watchListId
+        );
+        setUserWatchList(updatedWatchList);
+        console.log("Movie removed from watch list")
+    })
+}
+
 return <>
 <h2>Watch List</h2>
 <button onClick={() => navigate("/")}>Home</button>
@@ -32,6 +48,9 @@ return <>
                     <img src={movie.movie.image} alt={movie.movie.name} />
                     <header>{movie.movie.name}</header>
                     <footer>Description: {movie.movie.description}</footer>
+                    <button onClick={() => removeFromWatchList(movie.id)}>
+                Remove from Watch List
+            </button>
                 </section>
             }
         )
