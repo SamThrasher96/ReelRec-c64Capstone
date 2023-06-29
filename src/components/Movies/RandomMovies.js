674e08321b3c26d9de4f04db79a46c9b5bb1bcb5
Sam Thrasher <samthrasher9@gmail.com>
2023-06-29 14:10:26 -0500

    Made random filters into one button

--- src/components/Movies/RandomMovies.js
+++ src/components/Movies/RandomMovies.js
@@ -13,6 +13,7 @@ export const RandomMovieGenerator = () => {
   const localReelRecUser = localStorage.getItem("reelRec_user");
   const ReelRecUserObject = JSON.parse(localReelRecUser);
   const [userWatchListAndFavorites, setUserWatchListAndFavorites] = useState([]);
+  const [showFilters, setShowFilters] = useState(false); // Added state to toggle filters visibility
 
   const getMovies = () => {
     fetch("http://localhost:8088/movies?_expand=genre&_expand=mpaRating&_expand=streamingService")
@@ -26,38 +27,29 @@ export const RandomMovieGenerator = () => {
     getMovies()
   }, [])
 
-  useEffect(
-    () => {
-        fetch(`http://localhost:8088/genres`)
-            .then(response => response.json())
-            .then((genresArray) => {
-                setMovieGenre(genresArray)
-            })
-    },
-    []
-  )
+  useEffect(() => {
+    fetch(`http://localhost:8088/genres`)
+      .then(response => response.json())
+      .then((genresArray) => {
+        setMovieGenre(genresArray)
+      })
+  }, [])
 
-  useEffect(
-    () => {
-        fetch(`http://localhost:8088/streamingServices`)
-            .then(response => response.json())
-            .then((streamingServicesArray) => {
-                setStreamingService(streamingServicesArray)
-            })
-    },
-    []
-  )
+  useEffect(() => {
+    fetch(`http://localhost:8088/streamingServices`)
+      .then(response => response.json())
+      .then((streamingServicesArray) => {
+        setStreamingService(streamingServicesArray)
+      })
+  }, [])
 
-  useEffect(
-    () => {
-        fetch(`http://localhost:8088/mpaRatings`)
-            .then(response => response.json())
-            .then((mpaRatingArray) => {
-                setMpaRating(mpaRatingArray)
-            })
-    },
-    []
-  )
+  useEffect(() => {
+    fetch(`http://localhost:8088/mpaRatings`)
+      .then(response => response.json())
+      .then((mpaRatingArray) => {
+        setMpaRating(mpaRatingArray)
+      })
+  }, [])
 
   useEffect(() => {
     fetch("http://localhost:8088/userWatchListAndFavorites")
@@ -114,65 +106,83 @@ export const RandomMovieGenerator = () => {
     setFilterValue(event.target.value);
   }
 
+  const handleShowFilters = () => {
+    setShowFilters(!showFilters);
+  }
+
+  const handleGenerateWithFilters = () => {
+    generateRandomMovie();
+    setShowFilters(false);
+  }
+
   return (
     <>
-      <div>
-        <Button variant="contained" size="large" onClick={generateRandomMovie}>Generate Random Movie</Button>
-        <Button variant="contained" size="large" onClick={generateRandomMovieFromWatchList}>Generate Random Movie from Watch List</Button>
-      </div>
+<Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '20px', gap: '16px' }}>
+  <Button variant="contained" size="large" r="10 px" onClick={generateRandomMovie}>
+    Generate Random Movie
+  </Button>
+  <Button variant="contained" size="large" onClick={generateRandomMovieFromWatchList}>
+    Generate Random Movie from Watch List
+  </Button>
+  <Button variant="contained" size="large" onClick={handleShowFilters}>
+    Generate a random movie with filters.
+  </Button>
+</Box>
 
-      <div>
-        <h2>Get a random movie</h2>
+      {showFilters && (
         <div>
-          <label htmlFor="filterCategory">Filter Category:</label>
-          <select id="filterCategory" value={filterCategory} onChange={handleFilterCategoryChange}>
-            <option value="">Select Category</option>
-            <option value="genre">Genre</option>
-            <option value="streamingService">Streaming Service</option>
-            <option value="mpaRating">MPA Rating</option>
-          </select>
-        </div>
-
-        {filterCategory && (
           <div>
-            <label htmlFor="filterValue">Filter Value:</label>
-            {filterCategory === "genre" && (
-              <select id="filterValue" value={filterValue} onChange={handleFilterValueChange}>
-                <option value="">Select Genre</option>
-                {movieGenre.map(item => (
-                  <option value={item.id} key={item.id}>{item.genre}</option>
-                ))}
-              </select>
-            )}
-            {filterCategory === "streamingService" && (
-              <select id="filterValue" value={filterValue} onChange={handleFilterValueChange}>
-                <option value="" defaultValue>Select Streaming Service</option>
-                {streamingService.map(item => (
-                  <option value={item.id} key={item.id}>{item.service}</option>
-                ))}
-              </select>
-            )}
-            {filterCategory === "mpaRating" && (
-              <select id="filterValue" value={filterValue} onChange={handleFilterValueChange}>
-                <option value="" defaultValue>Select MPA Rating</option>
-                {mpaRating.map(item => (
-                  <option value={item.id} key={item.id}>{item.mpaRating}</option>
-                ))}
-              </select>
-            )}
+            <label htmlFor="filterCategory">Filter Category:</label>
+            <select id="filterCategory" value={filterCategory} onChange={handleFilterCategoryChange}>
+              <option value="">Select Category</option>
+              <option value="genre">Genre</option>
+              <option value="streamingService">Streaming Service</option>
+              <option value="mpaRating">MPA Rating</option>
+            </select>
           </div>
-        )}
 
-        {randomMovie && (
-          <section className="movie">
-            <img src={randomMovie.image} alt={randomMovie.name} />
-            <header>Random Movie:</header>
+          {filterCategory && (
             <div>
-              <Link to={`/Movies/RandomMovieDetails/${randomMovie.id}`}>Movie title: {randomMovie.name}</Link>
+              <label htmlFor="filterValue">Filter Value:</label>
+              {filterCategory === "genre" && (
+                <select id="filterValue" value={filterValue} onChange={handleFilterValueChange}>
+                  <option value="">Select Genre</option>
+                  {movieGenre.map(item => (
+                    <option value={item.id} key={item.id}>{item.genre}</option>
+                  ))}
+                </select>
+              )}
+              {filterCategory === "streamingService" && (
+                <select id="filterValue" value={filterValue} onChange={handleFilterValueChange}>
+                  <option value="" defaultValue>Select Streaming Service</option>
+                  {streamingService.map(item => (
+                    <option value={item.id} key={item.id}>{item.service}</option>
+                  ))}
+                </select>
+              )}
+              {filterCategory === "mpaRating" && (
+                <select id="filterValue" value={filterValue} onChange={handleFilterValueChange}>
+                  <option value="" defaultValue>Select MPA Rating</option>
+                  {mpaRating.map(item => (
+                    <option value={item.id} key={item.id}>{item.mpaRating}</option>
+                  ))}
+                </select>
+              )}
+              <Button variant="contained" size="large" onClick={handleGenerateWithFilters}>Generate</Button>
             </div>
-          </section>
-        )}
-      </div>
+          )}
+        </div>
+      )}
+
+      {randomMovie && (
+        <section className="movie">
+          <img src={randomMovie.image} alt={randomMovie.name} />
+          <header>Random Movie:</header>
+          <div>
+            <Link to={`/Movies/RandomMovieDetails/${randomMovie.id}`}>Movie title: {randomMovie.name}</Link>
+          </div>
+        </section>
+      )}
     </>
   )
 }
