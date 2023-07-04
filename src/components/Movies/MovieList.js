2fce254c37681be4dc9960953fdb08d1170e99f3
Sam Thrasher <samthrasher9@gmail.com>
2023-07-04 00:10:22 -0500

    Added scroll to top

--- src/components/Movies/MovieList.js
+++ src/components/Movies/MovieList.js
@@ -1,21 +1,37 @@
 import React, { useEffect, useState } from "react";
-import { Rating } from "@mui/material";
+import { Rating, IconButton } from "@mui/material";
 import "./movie.css";
-import { Button, Card, CardContent, CardMedia, Grid, Typography, CardActions } from "@mui/material";
-import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+  CardActions,
+  useTheme,
+} from "@mui/material";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 export const MovieList = ({ searchTermState }) => {
   const [movies, setMovies] = useState([]);
   const [filteredMovies, setFilteredMovies] = useState([]);
-  const [ratingValue, setRatingValue] = useState()
+  const [ratingValue, setRatingValue] = useState();
+  const [showScroll, setShowScroll] = useState(false);
   const localReelRecUser = localStorage.getItem("reelRec_user");
   const ReelRecUserObject = JSON.parse(localReelRecUser);
+  const theme = useTheme();
 
   const getMovies = () => {
-    fetch(`http://localhost:8088/movies?_expand=genre&_expand=mpaRating&_expand=streamingService`)
-      .then(response => response.json())
+    fetch(
+      `http://localhost:8088/movies?_expand=genre&_expand=mpaRating&_expand=streamingService`
+    )
+      .then((response) => response.json())
       .then((movieArray) => {
-        const sortedMovies = movieArray.sort((a, b) => a.name.localeCompare(b.name));
+        const sortedMovies = movieArray.sort((a, b) =>
+          a.name.localeCompare(b.name)
+        );
         setMovies(sortedMovies);
         setFilteredMovies(movieArray);
       });
@@ -26,8 +42,10 @@ export const MovieList = ({ searchTermState }) => {
   }, []);
 
   useEffect(() => {
-    const searchedMovies = movies.filter(movie => {
-      return movie.name.toLowerCase().startsWith(searchTermState.toLowerCase());
+    const searchedMovies = movies.filter((movie) => {
+      return movie.name
+        .toLowerCase()
+        .startsWith(searchTermState.toLowerCase());
     });
     setFilteredMovies(searchedMovies);
   }, [searchTermState]);
@@ -38,17 +56,17 @@ export const MovieList = ({ searchTermState }) => {
       movieId: movieId,
       userId: userId,
       watchList: true,
-      favorite: false
+      favorite: false,
     };
 
-    fetch(`http://localhost:8088/userWatchListAndFavorites`, {
+    fetch("http://localhost:8088/userWatchListAndFavorites", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(watchListObject)
+      body: JSON.stringify(watchListObject),
     })
-      .then(response => response.json())
+      .then((response) => response.json())
       .then(() => {
         console.log("Movie added to watch list");
       });
@@ -60,17 +78,17 @@ export const MovieList = ({ searchTermState }) => {
       movieId: movieId,
       userId: userId,
       watchList: false,
-      favorite: true
+      favorite: true,
     };
 
-    fetch(`http://localhost:8088/userWatchListAndFavorites`, {
+    fetch("http://localhost:8088/userWatchListAndFavorites", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(favoriteListObject)
+      body: JSON.stringify(favoriteListObject),
     })
-      .then(response => response.json())
+      .then((response) => response.json())
       .then(() => {
         console.log("Movie added to favorite list");
       });
@@ -78,7 +96,7 @@ export const MovieList = ({ searchTermState }) => {
 
   const deleteButton = (movieId) => {
     fetch(`http://localhost:8088/movies/${movieId}`, {
-      method: "DELETE"
+      method: "DELETE",
     })
       .then(() => {
         getMovies();
@@ -90,28 +108,52 @@ export const MovieList = ({ searchTermState }) => {
     const ratingObject = {
       movieId: movieId,
       userId: userId,
-      rating: ratingValue
+      rating: ratingValue,
     };
 
-    fetch(`http://localhost:8088/ratings`, {
+    fetch("http://localhost:8088/ratings", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(ratingObject)
-    }) 
-    .then(response => response.json())
+      body: JSON.stringify(ratingObject),
+    })
+      .then((response) => response.json())
       .then(() => {
         console.log("Movie rated");
       });
-  }
+  };
+
+  const handleScroll = () => {
+    if (window.pageYOffset > 300) {
+      setShowScroll(true);
+    } else {
+      setShowScroll(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <>
       <Grid container spacing={2} style={{ marginTop: "10px" }}>
         {filteredMovies.map((movie) => (
           <Grid item xs={12} sm={6} md={4} key={`movie--${movie.id}`}>
-            <Card sx={{ height: "100%", display: "flex", flexDirection: "column", margin: "10px" }}>
+            <Card
+              sx={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                margin: "10px",
+              }}
+            >
               <CardMedia
                 component="img"
                 height="650"
@@ -134,18 +176,18 @@ export const MovieList = ({ searchTermState }) => {
                 <Typography variant="body2" align="center">
                   {movie.description}
                 </Typography>
-                <Rating 
-                name="movieRating"
-                value={ratingValue}
-                onChange={( value) => setRatingValue(value)}
-                precision={0.5}
+                <Rating
+                  name="movieRating"
+                  value={ratingValue}
+                  onChange={(value) => setRatingValue(value)}
+                  precision={0.5}
                 >
                   <Typography>
                     Rated {ratingValue !== undefined ? ratingValue : 0} stars
                   </Typography>
-                  </Rating>
+                </Rating>
               </CardContent>
-              <CardActions sx={{ justifyContent: "center"}}>
+              <CardActions sx={{ justifyContent: "center" }}>
                 <Button
                   variant="contained"
                   color="error"
@@ -176,6 +218,24 @@ export const MovieList = ({ searchTermState }) => {
           </Grid>
         ))}
       </Grid>
+      {showScroll && (
+        <IconButton
+          onClick={scrollToTop}
+          style={{
+            position: "fixed",
+            bottom: "20px",
+            right: "20px",
+            zIndex: "99",
+          }}
+          sx={{
+            "&:hover": {
+              backgroundColor: theme.palette.primary.main,
+            },
+          }}
+        >
+          <KeyboardArrowUpIcon />
+        </IconButton>
+      )}
     </>
   );
 };
